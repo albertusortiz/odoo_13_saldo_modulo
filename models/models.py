6809743cf67087a747d422cda2621e19e536ffeb
albertusortiz <alberto.ortiz.vargas@gmail.com>
2021-04-06 22:22:42 -0500

    Creando restriccion para limitar a 5 movimientos para el usuario free.

--- models/models.py
+++ models/models.py
@@ -2,6 +2,7 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class Movimiento(models.Model):
   _name = "sa.movimiento" # sa_movimiento
@@ -45,6 +46,11 @@ class Movimiento(models.Model):
     type_move = vals.get("type_move", "")
     date = vals.get("date", "")
 
+    user = self.env.user
+    count_mov = user.count_movimientos
+    if count_mov >= 5 and user.has_group("saldo_modulo.res_groups_user_free"):
+      raise ValidationError("Solo puedes crear 5 movimientos por mes.")
+
     notas = """<p>Tipo de Movimiento: {}</p><p>Nombre: {}</p><p>Monto: {}</p><p>Fecha: {}</p>"""
     vals["notas"] = notas.format(type_move, name, amount, date)
     return super(Movimiento,self).create(vals)
@@ -87,12 +93,16 @@ class ResUsers(models.Model):
   movimiento_ids = fields.One2many("sa.movimiento","user_id")
   total_ingresos = fields.Float(string="Total de Ingresos", compute="_compute_movimientos")
   total_gastos = fields.Float(string="Total de Gastos", compute="_compute_movimientos")
+  count_movimientos = fields.Integer("Cantidad de movimientos por mes", compute="_compute_movimientos")
 
   @api.depends("movimiento_ids")
   def _compute_movimientos(self):
     for record in self:
       record.total_ingresos = sum(record.movimiento_ids.filtered(lambda r:r.type_move=='ingreso').mapped("amount"))
       record.total_gastos = sum(record.movimiento_ids.filtered(lambda r:r.type_move=='gasto').mapped("amount"))
+      mes = datetime.now().month
+      movs = record.movimiento_ids.filtered(lambda r: r.create_date.month == mes)
+      record.count_movimientos = len(movs)
 
   def mi_cuenta(self): #Devolviendo una accion de ventana
     return {
